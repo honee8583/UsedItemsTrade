1b7d979b45c403f64310685e3c273498c1df5fce
leedahoon <honee8583@naver.com>
2023-07-06 13:31:43 +0900

    UsedItemsTrade Remodeling Project Initial commit

---
+++ src/main/java/com/project/usedtrade/UsedItemsTradeApplication.java
@@ -0,0 +1,13 @@
+package com.project.usedtrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedItemsTradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsedItemsTradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/usedtrade/UsedItemsTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.usedtrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedItemsTradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
